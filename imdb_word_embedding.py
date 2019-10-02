1b942f43c1e06237683271abc8a15341a860715f
Li Chao <leetschau@gmail.com>
2019-10-02 08:46:01 +0800

    section 6.1 verified

---
+++ imdb_word_embedding.py
@@ -0,0 +1,182 @@
+# code listing 6-8
+import os
+
+imdb_dir = './aclImdb'
+train_dir = os.path.join(imdb_dir, 'train')
+
+labels = []
+texts = []
+
+for label_type in ['neg', 'pos']:
+    dir_name = os.path.join(train_dir, label_type)
+    for fname in os.listdir(dir_name):
+        if fname[-4:] == '.txt':
+            f = open(os.path.join(dir_name, fname))
+            texts.append(f.read())
+            f.close()
+            if label_type == 'neg':
+                labels.append(0)
+            else:
+                labels.append(1)
+
+# 共 25000 个文本文件，前一半在 neg 文件夹里，对应 label 为 0
+# 后一半在 pos 文件夹里，对应 label 为 1
+
+# code listing 6-9
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+import numpy as np
+
+maxlen = 100
+training_samples = 200
+validation_samples = 10000
+max_words = 10000
+
+tokenizer = Tokenizer(num_words=max_words)
+tokenizer.fit_on_texts(texts)
+sequences = tokenizer.texts_to_sequences(texts)
+# this is a list, whose element is a integer list
+# the length of the integer list is the word count in the corresponding txt file
+
+word_index = tokenizer.word_index
+print("Found %s unique tokens." % len(word_index))
+
+data = pad_sequences(sequences, maxlen=maxlen)
+
+labels = np.asarray(labels)
+print('Shape of data tensor:', data.shape)
+print('Shape of label tensor:', labels.shape)
+
+indices = np.arange(data.shape[0])
+np.random.shuffle(indices)   # 对 indices 做 in-place 修改，详细说明见 ?np.random.shuffle
+data = data[indices]         # 这样对变量做 in-place 修改是不好的
+labels = labels[indices]
+
+x_train = data[:training_samples]
+y_train = labels[:training_samples]
+x_val = data[training_samples : training_samples + validation_samples]
+y_val = labels[training_samples : training_samples + validation_samples]
+
+# code listing 6-10
+glove_dir = './glove.6B'
+
+embeddings_index = {}
+# glove.6B.100d.txt 文件每行包含101个元素，彼此用空格分隔，
+# 第一个是一个英文单词，后面是100个实数
+f = open(os.path.join(glove_dir, 'glove.6B.100d.txt'))
+for line in f:
+    values = line.split()
+    word = values[0]
+    coefs = np.asarray(values[1:], dtype='float32')
+    embeddings_index[word] = coefs
+f.close()
+print('Found %s word vectors' % len(embeddings_index))
+
+# code list 6-11
+embedding_dim = 100
+embedding_matrix = np.zeros((max_words, embedding_dim))
+for word, i in word_index.items():
+    if i < max_words:
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            embedding_matrix[i] = embedding_vector
+
+# code listing 6-12
+from keras.models import Sequential
+from keras.layers import Embedding, Flatten, Dense
+
+model = Sequential()
+model.add(Embedding(max_words, embedding_dim, input_length=maxlen))
+model.add(Flatten())
+model.add(Dense(32, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+model.summary()
+
+# code listing 6-13, 使用预训练嵌入层
+model.layers[0].set_weights([embedding_matrix])
+model.layers[0].trainable = False
+
+# code listing 6-14
+model.compile(optimizer='rmsprop',
+              loss='binary_crossentropy',
+              metrics=['acc'])
+history = model.fit(x_train, y_train, epochs=10, batch_size=32,
+        validation_data=(x_val, y_val))
+model.save_weights('pre_trained_glove_model.h5')
+
+# code listing 6-15
+import matplotlib.pyplot as plt
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(acc) + 1)
+plt.plot(epochs, acc, 'ro', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+plt.figure()
+
+plt.plot(epochs, loss, 'ro', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+plt.show()
+
+# code listing 6-16, 不使用预训练嵌入层
+model2 = Sequential()
+model2.add(Embedding(max_words, embedding_dim, input_length=maxlen))
+model2.add(Flatten())
+model2.add(Dense(32, activation='relu'))
+model2.add(Dense(1, activation='sigmoid'))
+model2.summary()
+
+model2.compile(optimizer='rmsprop', loss='binary_crossentropy',
+        metrics=['acc'])
+history2 = model2.fit(x_train, y_train, epochs=10, batch_size=32,
+        validation_data=[x_val, y_val])
+
+acc2 = history2.history['acc']
+val_acc2 = history2.history['val_acc']
+loss2 = history2.history['loss']
+val_loss2 = history2.history['val_loss']
+
+epochs2 = range(1, len(acc2) + 1)
+plt.plot(epochs2, acc2, 'ro', label='Training acc')
+plt.plot(epochs2, val_acc2, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+plt.figure()
+
+plt.plot(epochs2, loss2, 'ro', label='Training loss')
+plt.plot(epochs2, val_loss2, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+plt.show()
+
+# code listing 6-17 对测试集数据分词
+test_dir = os.path.join(imdb_dir, 'test')
+
+labels = []
+texts = []
+
+for label_type in ['neg', 'pos']:
+    dir_name = os.path.join(test_dir, label_type)
+    for fname in sorted(os.listdir(dir_name)):
+        if fname[-4:] == '.txt':
+            f = open(os.path.join(dir_name, fname))
+            texts.append(f.read())
+            f.close()
+            if label_type == 'neg':
+                labels.append(0)
+            else:
+                labels.append(1)
+
+sequences = tokenizer.texts_to_sequences(texts)
+x_test = pad_sequences(sequences, maxlen=maxlen)
+y_test = np.asarray(labels)
+
+# code listing 6-18
+model.load_weights('pre_trained_glove_model.h5')
+model.evaluate(x_test, y_test)
